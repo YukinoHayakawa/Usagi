54828398a4d546f195883ce6d9959884d8bf4b8c
Yukino Hayakawa <tennencoll@gmail.com>
2017-03-29 23:43:53 +0800

    use BOOST_PP_REPEAT

--- char_list.hpp
+++ char_list.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+
 #include "apply_first_n_value.hpp"
 #include "string_literal.hpp"
 
@@ -65,26 +68,12 @@ using char_list = basic_char_list<char, Ch...>;
 
 }
 
-#define YUKI_STRCHR(string, N) ((N >= 0 && N < sizeof(string)) ? string[N] : 0)
+// a comma is prepended if N is not 0.
+#define YUKI_STRCHR(z, N, string) BOOST_PP_COMMA_IF(N) ((N >= 0 && N < sizeof(string)) ? string[N] : 0)
 
 #define YUKI_MAKE_CHAR_LIST(string) \
     ::yuki::char_list< \
-        YUKI_STRCHR(string, 0),  YUKI_STRCHR(string, 1),  YUKI_STRCHR(string, 2),  YUKI_STRCHR(string, 3),  \
-        YUKI_STRCHR(string, 4),  YUKI_STRCHR(string, 5),  YUKI_STRCHR(string, 6),  YUKI_STRCHR(string, 7),  \
-        YUKI_STRCHR(string, 8),  YUKI_STRCHR(string, 9),  YUKI_STRCHR(string, 10), YUKI_STRCHR(string, 11), \
-        YUKI_STRCHR(string, 12), YUKI_STRCHR(string, 13), YUKI_STRCHR(string, 14), YUKI_STRCHR(string, 15), \
-        YUKI_STRCHR(string, 16), YUKI_STRCHR(string, 17), YUKI_STRCHR(string, 18), YUKI_STRCHR(string, 19), \
-        YUKI_STRCHR(string, 20), YUKI_STRCHR(string, 21), YUKI_STRCHR(string, 22), YUKI_STRCHR(string, 23), \
-        YUKI_STRCHR(string, 24), YUKI_STRCHR(string, 25), YUKI_STRCHR(string, 26), YUKI_STRCHR(string, 27), \
-        YUKI_STRCHR(string, 28), YUKI_STRCHR(string, 29), YUKI_STRCHR(string, 30), YUKI_STRCHR(string, 31), \
-        YUKI_STRCHR(string, 32), YUKI_STRCHR(string, 33), YUKI_STRCHR(string, 34), YUKI_STRCHR(string, 35), \
-        YUKI_STRCHR(string, 36), YUKI_STRCHR(string, 37), YUKI_STRCHR(string, 38), YUKI_STRCHR(string, 39), \
-        YUKI_STRCHR(string, 40), YUKI_STRCHR(string, 41), YUKI_STRCHR(string, 42), YUKI_STRCHR(string, 43), \
-        YUKI_STRCHR(string, 44), YUKI_STRCHR(string, 45), YUKI_STRCHR(string, 46), YUKI_STRCHR(string, 47), \
-        YUKI_STRCHR(string, 48), YUKI_STRCHR(string, 49), YUKI_STRCHR(string, 50), YUKI_STRCHR(string, 51), \
-        YUKI_STRCHR(string, 52), YUKI_STRCHR(string, 53), YUKI_STRCHR(string, 54), YUKI_STRCHR(string, 55), \
-        YUKI_STRCHR(string, 56), YUKI_STRCHR(string, 57), YUKI_STRCHR(string, 58), YUKI_STRCHR(string, 59), \
-        YUKI_STRCHR(string, 60), YUKI_STRCHR(string, 61), YUKI_STRCHR(string, 62), YUKI_STRCHR(string, 63)  \
+        BOOST_PP_REPEAT(64, YUKI_STRCHR, string) \
     >::c_string_t \
 /**/
 
