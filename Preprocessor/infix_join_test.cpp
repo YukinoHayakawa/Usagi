b98e3b45779ad07633e0a30667f4d245cd9ccffd
Yukino Hayakawa <tennencoll@gmail.com>
2017-04-06 22:22:26 +0800

    add YUKI_TAIL_UNPACK macro, add base cases for YUKI_INFIX_JOIN and YUKI_MAKE_NESTED_NAMESPACE

--- Preprocessor/infix_join_test.cpp
+++ Preprocessor/infix_join_test.cpp
@@ -4,3 +4,7 @@
 YUKI_MAKE_NESTED_NAMESPACE((foo, bar, baz), (constexpr int hello = 5;))
 
 static_assert(YUKI_INFIX_JOIN(::, foo, bar, baz, hello) == 5, "infix_join");
+
+// root namespace test
+constexpr int world = 10;
+static_assert(YUKI_INFIX_JOIN(::) world == 10, "infix_join in root ns");
--- Preprocessor/make_nested_namespace_test.cpp
+++ Preprocessor/make_nested_namespace_test.cpp
@@ -3,3 +3,7 @@
 YUKI_MAKE_NESTED_NAMESPACE((foo, bar, baz), (constexpr int hello = 5;))
 
 static_assert(foo::bar::baz::hello == 5, "make_nested_ns");
+
+YUKI_MAKE_NESTED_NAMESPACE((), (constexpr int world = 10;))
+
+static_assert(world == 10, "make_nested_ns in root ns");
