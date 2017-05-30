20ebd31e2527229e2d9ab10522dab784eee8632a
Yukino Hayakawa <tennencoll@gmail.com>
2017-05-30 13:57:18 +0800

    update comments

--- detail/class_traits_common.hpp
+++ detail/class_traits_common.hpp
@@ -14,27 +14,30 @@
  * \param _namespaces The namespace the class is in, wrapped in a tuple with each scope separated by commas.
  * \param _identifier The name of the class or template.
  * \param _elements Reflection information.
- * \param _macro_make_type A macro of the form of M(_namespaces, _identifier), which defines _reflecting_t when invoked.
- * \param _macro_make_class_decl A macro of the form of M(_namespaces, _identifier),
+ * \param _macro_reflected_type_decl A macro of the form of M(_namespaces, _identifier), which defines _reflecting_t when invoked.
+ * \param _macro_trait_class_decl A macro of the form of M(_namespaces, _identifier),
  * which generates the class header of information wrapper class when invoked.
- * \param _macro_make_class_type A macro of the form of M(_namespaces, _identifier),
+ * \param _macro_reflected_type_nested_decl A macro of the form of M(_namespaces, _identifier),
  * which defines _reflecting_t inside the information wrapper class when invoked.
  */
-#define YUKI_REFL_BASE(_namespaces, _identifier, _elements, _macro_make_type, _macro_make_class_decl, _macro_make_class_type) \
-/* the sole purpose of this nested namespace, is to provide a scope for declaring a _reflecting_t, so that it can be uniquely referred to. */ \
+#define YUKI_REFL_BASE(_namespaces, _identifier, _elements, _macro_reflected_type_decl, _macro_trait_class_decl, _macro_reflected_type_nested_decl) \
+/* use a nested namespace to provide a scope for declaring a _reflecting_t for each class, \
+   avoiding passing the class name to each trait definition macro. */ \
 namespace yuki { namespace reflection { namespace detail { namespace meta { YUKI_MAKE_NESTED_NAMESPACE((YUKI_HEAD_UNPACK _namespaces _identifier), (\
 YUKI_USE_NAMESPACE(_namespaces) /* introduce identifiers from the namespace containing the class */ \
-_macro_make_type(_namespaces, _identifier); \
-/* default traits, if user does not use the corresponding defining macros, these will be used */ \
+_macro_reflected_type_decl(_namespaces, _identifier); \
+/* default traits of the class, which will be used if user does not define */ \
 template <typename T> struct base_list { typedef boost::mpl::vector<> _base_list_t; }; \
 template <typename T> struct member_list { typedef boost::mpl::vector<> _member_list_t; }; \
 template <typename T> struct nested_type_list { typedef boost::mpl::vector<> _nested_type_list_t; }; \
-YUKI_UNPACK _elements /* template specializations, inside which only _reflecting_t is used */ \
+/* template specializations, using _reflecting_t to refer to the class */ \
+/* each implementation may use different macros */ \
+YUKI_UNPACK _elements \
 )) }}}} /* namespace yuki::reflection::detail::meta */ \
 namespace yuki { namespace reflection { namespace detail { \
-_macro_make_class_decl(_namespaces, _identifier) { \
+_macro_trait_class_decl(_namespaces, _identifier) { \
 private: \
-    _macro_make_class_type(_namespaces, _identifier); \
+    _macro_reflected_type_nested_decl(_namespaces, _identifier); \
     YUKI_REFL_IMPL_DEFINE_LIST(_namespaces, _identifier, base_list); \
     YUKI_REFL_IMPL_DEFINE_LIST(_namespaces, _identifier, member_list); \
     YUKI_REFL_IMPL_DEFINE_LIST(_namespaces, _identifier, nested_type_list); \
