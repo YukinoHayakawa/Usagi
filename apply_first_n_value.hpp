12908881389615fe645192e0f7151b83c11c6e82
Yukino Hayakawa <tennencoll@gmail.com>
2017-06-11 16:18:53 +0800

    apply_first_n_value: allow insufficient arguments (like the take function in Haskell)

--- apply_first_n_value.hpp
+++ apply_first_n_value.hpp
@@ -6,24 +6,26 @@ namespace yuki
 {
 
 /**
- * \brief Apply first N arguments in a non-type parameter pack to a template. This is the primary template.
- * \tparam N Number of arguments in Args... should be applied to Target.
- * \tparam T The type of values being applied.
- * \tparam Target To which template the values will be applied.
+ * \brief Apply first N arguments in a non-type parameter pack to a template.
+ * This is the primary template.
+ * \tparam N Number of arguments in Args... applied to Target. If N > sizeof...(Args), all arguments
+ * will be used.
+ * \tparam T The type of arguments.
+ * \tparam Target A template accepting N parameters of T.
  * Template not directly fitting the signature may be aliased using using directive. e.g.
  * template <typename T, T...> struct foo { };
  * template <int...Ch> using bar = foo<int, Ch...>;
- * Then use bar as Target.
- * \tparam Args A non-type template parameter pack of which the first N values will be applied to Target
+ * \tparam Args A non-type template parameter pack whose first N values will be applied to Target
  */
-template <size_t N, typename T, template <T...> typename Target, T... Args>
+template <std::size_t N, typename T, template <T...> typename Target, T... Args>
 struct apply_first_n_value;
 
 /**
- * \brief Specialized for applying no arguments to Target.
- * \tparam T 
- * \tparam Target 
- */
+* \brief Specialization for the applying no arguments to the template.
+* \tparam N
+* \tparam T
+* \tparam Target
+*/
 template <typename T, template <T...> typename Target>
 struct apply_first_n_value<0, T, Target>
 {
@@ -31,11 +33,10 @@ struct apply_first_n_value<0, T, Target>
 };
 
 /**
- * \brief Specialized for terminating the applying action.
+ * \brief Specialization for applying no arguments to the template from a parameter pack having
+ * one or more arguments.
  * \tparam T 
- * \tparam Target This is necessary to make this specialization more special than the next one.
- * \tparam First 
- * \tparam Args 
+ * \tparam Target 
  */
 template <typename T, template <T...> typename Target, T First, T... Args>
 struct apply_first_n_value<0, T, Target, First, Args...>
@@ -44,21 +45,31 @@ struct apply_first_n_value<0, T, Target, First, Args...>
 };
 
 /**
- * \brief Specialized for expanding the parameter pack.
+ * \brief Specialization for insufficient arguments.
+ * \tparam N 
+ * \tparam T 
+ * \tparam Target 
+ */
+template <std::size_t N, typename T, template <T...> typename Target>
+struct apply_first_n_value<N, T, Target>
+{
+    typedef Target<> type;
+};
+
+/**
+ * \brief Speacialization as recursion start point.
  * \tparam N 
  * \tparam T 
  * \tparam Target 
  * \tparam First 
  * \tparam Others 
  */
-template <size_t N, typename T, template <T...> typename Target, T First, T... Others>
+template <std::size_t N, typename T, template <T...> typename Target, T First, T... Others>
 struct apply_first_n_value<N, T, Target, First, Others...>
 {
-    static_assert(N <= 1 + sizeof...(Others), "paramater pack index out of bound");
-
 private:
-    template <T...Others2>
-    using apply_next_t = Target<First, Others2...>;
+    template <T...NextOthers>
+    using apply_next_t = Target<First, NextOthers...>;
     
 public:
     typedef typename apply_first_n_value<N - 1, T, apply_next_t, Others...>::type type;
