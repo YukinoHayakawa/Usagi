091db4f462d00885bc45c86bfc9362ba9f11fa14
Yukino Hayakawa <tennencoll@gmail.com>
2017-07-23 18:03:05 +0800

    use new GraphicsDevice interface

--- Editor/main2.cpp
+++ Editor/main2.cpp
@@ -1,99 +1,40 @@
-#include <GL/glew.h>
-#include <Usagi/Engine/Runtime/DevicePlatform/Win32DevicePlatform.hpp>
-#include <Usagi/Engine/Runtime/RenderWindow.hpp>
-#include <Usagi/Engine/Runtime/GraphicsInterface/OpenGL/Shader/OpenGLShader.hpp>
-#include <Usagi/Engine/Runtime/GraphicsInterface/OpenGL/Shader/OpenGLProgram.hpp>
+#include <Usagi/Engine/Runtime/Window/Win32Window.hpp>
+#include <Usagi/Engine/Runtime/GraphicsDevice/OpenGL/Context/WGLContext.hpp>
+#include <Usagi/Engine/Runtime/GraphicsDevice/OpenGL/OpenGLGraphicsDevice.hpp>
+#include <Usagi/Engine/Runtime/Subsystem/DebugDrawManager.hpp>
+#include <Usagi/Engine/Runtime/Clock.hpp>
+
+
 
 int main(int argc, char *argv[])
 {
     using namespace yuki;
-//
-//    Win32Window w("UsagiEditor", 1280, 720);
-//    w.show();
-//    w.loop();
 
     try
     {
-        Win32DevicePlatform dev;
-        auto rwnd = dev.createRenderWindow("UsagiEditor", 1280, 720);
-        rwnd->show();
-        rwnd->setCurrent();
+        auto window = std::make_shared<Win32Window>("UsagiEditor", 1280, 720);
+        auto device = std::make_shared<OpenGLGraphicsDevice>(std::make_shared<WGLContext>(window->getDeviceContext()));
 
-        OpenGLShader vtxshader(GL_VERTEX_SHADER, R"(
-            #version 330 core
+        window->show();
+        device->setContextCurrent();
 
-            // Input vertex data, different for all executions of this shader.
-            layout(location = 0) in vec3 vertexPosition_modelspace;
+        DebugDrawManager dd(*device.get());
+        Clock master;
 
-            void main()
-            {
-                gl_Position.xyz = vertexPosition_modelspace;
-                gl_Position.w = 1.0;
+        while(true)
+        {
+            master.tick();
 
-            }
-        )"), fragshader(GL_FRAGMENT_SHADER, R"(
-            #version 330 core
+            window->processEvents();
 
-            // Ouput data
-            out vec3 color;
+            device->clearCurrentFrameBuffer();
 
-            void main()
-            {
-	            // Output color = red 
-	            color = vec3(1,0,0);
-            }
-        )");
-
-        OpenGLProgram prog;
-        prog.attachShader(vtxshader);
-        prog.attachShader(fragshader);
-        prog.link();
-
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        GLuint VertexArrayID;
-        glGenVertexArrays(1, &VertexArrayID);
-        glBindVertexArray(VertexArrayID);
-
-        // An array of 3 vectors which represents 3 vertices
-        static const GLfloat g_vertex_buffer_data[] = {
-            -1.0f, -1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            0.0f,  1.0f, 0.0f,
-        };
-
-        // This will identify our vertex buffer
-        GLuint vertexbuffer;
-        // Generate 1 buffer, put the resulting identifier in vertexbuffer
-        glGenBuffers(1, &vertexbuffer);
-        // The following commands will talk about our 'vertexbuffer' buffer
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        // Give our vertices to OpenGL.
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            (void*)0            // array buffer offset
-        );
-        // Draw the triangle !
-        glUseProgram(prog.getId());
-
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-        glDisableVertexAttribArray(0);
-
-        rwnd->swapBuffer();
-
-        rwnd->processEvents();
+            dd.render(*device.get(), master);
+            
+            device->swapFrameBuffers();
+        }
     }
-    catch(std::exception &e)
+    catch(std::exception &)
     {
         throw;
     }
