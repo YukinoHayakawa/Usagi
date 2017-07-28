bbaed46a7e8254d00e5b99634d8b18f34586265b
Yukino Hayakawa <tennencoll@gmail.com>
2017-07-28 12:56:50 +0800

    add Noncopyable

---
+++ Engine/Utility/Noncopyable.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace yuki
+{
+
+class Noncopyable
+{
+public:
+    Noncopyable() = default;
+    Noncopyable(const Noncopyable &other) = delete;
+    Noncopyable(Noncopyable &&other) noexcept = delete;
+    Noncopyable & operator=(const Noncopyable &other) = delete;
+    Noncopyable & operator=(Noncopyable &&other) noexcept = delete;
+};
+
+}
