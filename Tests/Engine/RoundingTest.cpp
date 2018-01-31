2392b6ebcb35e212c7d5e504a49c480d15a56069
Yukino Hayakawa <tennencoll@gmail.com>
2018-01-31 22:18:47 +0800

    add tests for rountUpUnsigned

--- Tests/Engine/RoundingTest.cpp
+++ Tests/Engine/RoundingTest.cpp
@@ -8,3 +8,12 @@ TEST(RoundingTest, SpanningPages) {
     EXPECT_EQ(yuki::utility::calculateSpanningPages(16, 4), 4);
     EXPECT_EQ(yuki::utility::calculateSpanningPages(17, 4), 5);
 }
+
+TEST(RoundingTest, RoundUpUnsigned) {
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(0, 1), 0);
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(1, 1), 1);
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(1, 4), 4);
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(0, 16), 0);
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(3, 16), 16);
+    EXPECT_EQ(yuki::utility::roundUpUnsigned(16, 16), 16);
+}
