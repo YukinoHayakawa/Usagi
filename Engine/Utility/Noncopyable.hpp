715f0ccadd0ff04cf4951901abcb34cba4f4e1c2
Yukino Hayakawa <tennencoll@gmail.com>
2018-06-14 23:19:27 +0800

    fix missing special members of Noncopyable

--- Engine/Utility/Noncopyable.hpp
+++ Engine/Utility/Noncopyable.hpp
@@ -3,8 +3,10 @@
 namespace yuki
 {
 /**
- * \brief Note that classes deriving from a class with deleted special
- * member functions will not implicitly generate them either.
+ * \brief Remove the implicit copy and move constructors and assgnment
+ * operators from derived class.
+ * Derive privately to prevent unintended casts toward this class and
+ * avoid unnecessary virtual destructors.
  */
 class Noncopyable
 {
@@ -12,6 +14,8 @@ public:
     Noncopyable() = default;
     ~Noncopyable() = default;
     Noncopyable(const Noncopyable &other) = delete;
+    Noncopyable(Noncopyable &&other) = delete;
     Noncopyable & operator=(const Noncopyable &other) = delete;
+    Noncopyable & operator=(Noncopyable &&other) = delete;
 };
 }
