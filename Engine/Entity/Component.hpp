073e8cd708dc7078724d3aa1ea299e7e6ffa530f
Yukino Hayakawa <tennencoll@gmail.com>
2018-07-02 14:27:30 +0800

    update project files

--- Engine/Entity/Component.hpp
+++ Engine/Entity/Component.hpp
@@ -17,9 +17,9 @@ public:
     /**
      * \brief Identify the type of component. A component may have a base type
      * and a subsystem may create derived component from it. This methods
-     * returns the base type of the component.
-     * This method can be implemented in the base type such as GraphicsComponent
-     * as final.
+     * returns the base type of the component. One entity can only have one
+     * component of a specific base type such as GraphicsComponent.
+     * This method can be implemented in the base type as final.
      * \return 
      */
     virtual const std::type_info & getBaseTypeInfo() = 0;
--- ReCraft/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <Usagi/Engine/Extension/Win32/Win32Window.hpp>
-
-using namespace yuki;
-
-int main(int argc, char *argv[])
-{
-    Win32Window window;
-}
