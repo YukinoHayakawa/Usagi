6956f2a86d1c0c007158258e8433857e9be11b30
Yukino Hayakawa <tennencoll@gmail.com>
2018-08-19 16:16:53 +0800

    add EnumTranslator

---
+++ Engine/Utility/EnumTranslator.hpp
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include <cstddef>
+#include <array>
+#include <algorithm>
+
+#include <Usagi/Preprocessor/unpack.hpp>
+
+namespace usagi
+{
+template <
+    typename SrcEnum,
+    typename DestEnum
+>
+struct EnumTranslationTraits;
+
+template <
+    typename SrcEnum,
+    typename DestEnum
+>
+struct EnumTranslator
+{
+    // todo c++20 constexpr std::find
+    static /*constexpr*/ DestEnum translate(const SrcEnum e)
+    {
+        using Traits = EnumTranslationTraits<SrcEnum, DestEnum>;
+        auto i = std::find(Traits::src.begin(), Traits::src.end(), e);
+        if(i != Traits::src.end())
+            return Traits::dest[i - Traits::src.begin()];
+        throw std::runtime_error("Could not translate the enum.");
+    }
+};
+}
+
+#define USAGI_ENUM_TRANSLATION(src_t, dst_t, num_elem, src_elems, dst_elems) \
+template <> \
+struct ::usagi::EnumTranslationTraits<src_t, dst_t> \
+{ \
+    static constexpr std::array<src_t, num_elem> src { USAGI_UNPACK src_elems }; \
+    static constexpr std::array<dst_t, num_elem> dest { USAGI_UNPACK dst_elems }; \
+}; \
+template <> \
+struct ::usagi::EnumTranslationTraits<dst_t, src_t> \
+{ \
+    static constexpr std::array<dst_t, num_elem> src { USAGI_UNPACK dst_elems }; \
+    static constexpr std::array<src_t, num_elem> dest { USAGI_UNPACK src_elems }; \
+}; \
+/*constexpr*/ inline dst_t translate(const src_t e) \
+{ \
+    return ::usagi::EnumTranslator<src_t, dst_t>::translate(e); \
+} \
+/*constexpr*/ inline src_t translate(const dst_t e) \
+{ \
+    return ::usagi::EnumTranslator<dst_t, src_t>::translate(e); \
+} \
+/**/
---
+++ Tests/Test_All/test_enum_translation.cpp
@@ -0,0 +1,43 @@
+﻿#include <gtest/gtest.h>
+
+#include <Usagi/Engine/Utility/EnumTranslator.hpp>
+
+enum class A
+{
+    a, b, c, d, e
+};
+
+enum class B
+{
+    e, d, c, b, a
+};
+
+USAGI_ENUM_TRANSLATION(
+    A, B, 4,
+    (A::a, A::b, A::c, A::d),
+    (B::a, B::b, B::c, B::d)
+)
+
+TEST(EnumTranslationTest, BasicTranslationTest)
+{
+    using namespace usagi;
+
+    // todo c++20 constexpr std::find
+    /*
+    static_assert(translate(A::a) == B::a);
+    static_assert(translate(A::b) == B::b);
+    static_assert(translate(A::c) == B::c);
+    static_assert(translate(A::d) == B::d);
+    */
+
+    EXPECT_EQ(translate(A::a), B::a);
+    EXPECT_EQ(translate(A::b), B::b);
+    EXPECT_EQ(translate(A::c), B::c);
+    EXPECT_EQ(translate(A::d), B::d);
+    EXPECT_THROW(translate(A::e), std::runtime_error);
+
+    EXPECT_EQ(translate(B::a), A::a);
+    EXPECT_EQ(translate(B::b), A::b);
+    EXPECT_EQ(translate(B::c), A::c);
+    EXPECT_EQ(translate(B::d), A::d);
+}
