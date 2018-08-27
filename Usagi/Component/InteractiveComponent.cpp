57d877363117814e40875c92f0d031736cbb5874
Yukino Hayakawa <tennencoll@gmail.com>
2018-08-27 23:29:57 +0800

    remove obsolete code

--- Usagi/Component/InteractiveComponent.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "InteractiveComponent.hpp"
-
-yuki::InteractiveComponent::InteractiveComponent(std::shared_ptr<Mouse> mouse, std::shared_ptr<Keyboard> keyboard)
-    : mMouse { std::move(mouse) }
-    , mKeyboard { std::move(keyboard) }
-{
-}
