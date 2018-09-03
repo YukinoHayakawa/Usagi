59eb51bcd87904592412ad8a03822c6ae6fc4141
Yukino Hayakawa <tennencoll@gmail.com>
2018-09-03 15:43:40 +0800

    fix orthogonal camera matrix

--- Usagi/Camera/OrthogonalCamera.cpp
+++ Usagi/Camera/OrthogonalCamera.cpp
@@ -12,7 +12,7 @@ void usagi::OrthogonalCamera::setOrthogonal(
     mat <<
         2 / (right - left), 0, 0, -(right + left) / (right - left),
         0, 0, -2 / (top - bottom), (top + bottom) / (top - bottom),
-        0, 1 / (far - near), 0, -(far + near) / (far - near),
+        0, 1 / (far - near), 0, -near / (far - near),
         0, 0, 0, 1;
 
     mLocalToNdc = mat;
