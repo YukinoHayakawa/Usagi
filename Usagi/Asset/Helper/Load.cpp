b18f85065ff4a4bfe447246c537f8c4ed8455ef6
Yukino Hayakawa <tennencoll@gmail.com>
2018-09-30 20:58:37 +0800

    add asset loading helper

---
+++ Usagi/Asset/Helper/Load.cpp
@@ -0,0 +1,15 @@
+﻿#include "Load.hpp"
+
+#include <Usagi/Asset/AssetRoot.hpp>
+#include <Usagi/Asset/Converter/GpuImageAssetConverter.hpp>
+#include <Usagi/Game/Game.hpp>
+#include <Usagi/Runtime/Runtime.hpp>
+
+std::shared_ptr<usagi::GpuImage> usagi::loadTexture(
+    Game *game,
+    const std::string &locator)
+{
+    return game->assets()->res<GpuImageAssetConverter>(
+        locator, game->runtime()->gpu()
+    );
+}
---
+++ Usagi/Asset/Helper/Load.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <memory>
+#include <string>
+
+namespace usagi
+{
+class Game;
+class GpuImage;
+
+std::shared_ptr<GpuImage> loadTexture(Game *game, const std::string &locator);
+}
