d3248617aa6b6698b38b02acdfb49b6115998d14
Yukino Hayakawa <tennencoll@gmail.com>
2019-02-11 19:39:06 +0800

    add delegated debug draw component

---
+++ Usagi/Extension/DebugDraw/DelegatedDebugDrawComponent.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <functional>
+
+#include "DebugDrawComponent.hpp"
+
+namespace usagi
+{
+struct DelegatedDebugDrawComponent : DebugDrawComponent
+{
+    using DrawFunction = std::function<void(dd::ContextHandle ctx)>;
+    DrawFunction draw_func;
+
+    DelegatedDebugDrawComponent() = default;
+
+    DelegatedDebugDrawComponent(DrawFunction draw_func)
+        : draw_func(std::move(draw_func))
+    {
+    }
+
+    void draw(dd::ContextHandle ctx) override
+    {
+        draw_func(ctx);
+    }
+};
+}
