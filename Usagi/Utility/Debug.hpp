8182dc678f661ea1e5fe2361f83d80637fadd6e9
Yukino Hayakawa <tennencoll@gmail.com>
2019-06-03 15:55:33 +0800

    add debugBreak()

---
+++ Usagi/Utility/Debug.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+namespace usagi
+{
+inline void debugBreak()
+{
+#ifdef _WIN32
+	if(IsDebuggerPresent())
+	{
+		__debugbreak();
+	}
+#else
+	__builtin_trap();
+#endif
+}
+}
