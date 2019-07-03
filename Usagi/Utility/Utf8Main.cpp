f76d5ea4984dcd144fca7d4362e77e6585af8063
Yukino Hayakawa <tennencoll@gmail.com>
2019-07-03 22:36:09 +0800

    add unified main function with utf8 args

---
+++ Usagi/Utility/Utf8Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <string>
+
+#include <utf8.h>
+
+namespace usagi
+{
+std::vector<std::string> argsFromMain(int argc, wchar_t *argv[])
+{
+    std::vector<std::string> args;
+    args.reserve(argc);
+    for(auto i = 1; i < argc; ++i)
+    {
+        std::wstring_view u16_arg = argv[i];
+        std::string u8_arg;
+        utf8::utf16to8(
+            u16_arg.begin(), u16_arg.end(),
+            std::back_inserter(u8_arg)
+        );
+        args.push_back(std::move(u8_arg));
+    }
+    return args;
+}
+
+std::vector<std::string> argsFromMain(int argc, char *argv[])
+{
+    std::vector<std::string> args;
+    args.reserve(argc);
+    for(auto i = 1; i < argc; ++i)
+        args.push_back(argv[i]);
+    return args;
+}
+}
---
+++ Usagi/Utility/Utf8Main.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <vector>
+#include <string>
+
+// implement this function to get uniformed main function with UTF-8 inputs.
+int usagi_main(const std::vector<std::string> &args);
+
+namespace usagi
+{
+std::vector<std::string> argsFromMain(int argc, wchar_t *argv[]);
+std::vector<std::string> argsFromMain(int argc, char *argv[]);
+}
+
+#ifdef _WIN32
+int wmain(int argc, wchar_t *argv[])
+{
+    const auto args = usagi::argsFromMain(argc, argv);
+    return usagi_main(args);
+}
+#else
+int main(int argc, char *argv[])
+{
+    const auto args = usagi::argsFromMain(argc, argv);
+    return usagi_main(args);
+}
+#endif
