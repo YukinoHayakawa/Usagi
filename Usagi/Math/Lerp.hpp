ccb4b3cefd20511239e2392e85287c754a9bc1e5
Yukino Hayakawa <tennencoll@gmail.com>
2019-10-24 10:56:19 +0800

    add lerp/remap()

--- Usagi/Math/Lerp.hpp
+++ Usagi/Math/Lerp.hpp
@@ -13,4 +13,16 @@ R lerpCoefficient(V v, V v0, V v1)
 {
     return static_cast<R>(static_cast<R>(v) - v0) / (static_cast<R>(v1) - v0);
 }
+
+template <typename T>
+constexpr T alpha(T v, T a, T b)
+{
+    return (v - a) / (b - a);
+}
+
+template <typename T>
+constexpr T remap(T a, T b, T c, T d, T v)
+{
+    return lerp(alpha(v, a, b), c, d);
+}
 }
