fc482986df6d24fbe70bf6e80e763d0173aac681
Yukino Hayakawa <tennencoll@gmail.com>
2020-02-05 21:07:42 +0800

    add virtualmemoryallocator

---
+++ Usagi/Experimental/v2/Runtime/Platform/Kernel.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <cstddef>
+
+// These functions are not defined in the core library and must be linked
+// with runtime implementation library for the targeted platform.
+
+namespace usagi::platform::kernel
+{
+namespace virtual_memory
+{
+// Get the page size in bytes.
+std::size_t page_size();
+std::size_t round_up_to_page_size(std::size_t size_bytes);
+
+// Reserve a virtual address range. If commit is true, the range is also
+// committed.
+// Throws std::bad_alloc if the allocation fails.
+void * allocate(std::size_t size_bytes, bool commit);
+
+// Commit a range of virtual address. The [ptr, ptr + size_bytes) must
+// be in a valid range returned by allocate().
+void commit(void *ptr, std::size_t size_bytes);
+
+// Decommit a range of virtual address. The [ptr, ptr + size_bytes) must
+// be in a valid range returned by allocate().
+void decommit(void *ptr, std::size_t size_bytes);
+
+// Free a range of virtual address. The [ptr, ptr + size_bytes) must be
+// exactly a valid range returned by virtualMemoryAllocate(). The range will be
+// decommitted and no longer accessible.
+void free(void *ptr, std::size_t size_bytes);
+}
+}
