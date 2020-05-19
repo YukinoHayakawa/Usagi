232af0161b6f567822c6a58a233cf7e1b7413333
Yukino Hayakawa <tennencoll@gmail.com>
2020-05-19 14:39:53 +0800

    added a compile time error emit helper

---
+++ Library/Meta/CompileTimeError.hpp
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+namespace usagi
+{
+template <typename T, typename AdditionalInfo, T SucceedValue>
+struct CompileTimeError
+{
+    struct ErrorCode
+    {
+        T error_code;
+        AdditionalInfo info;
+
+        constexpr ErrorCode(
+            T error_code = SucceedValue,
+            AdditionalInfo info = { })
+            : error_code(error_code)
+            , info(info)
+        {
+        }
+
+        constexpr operator bool() const
+        {
+            return error_code == SucceedValue;
+        }
+    };
+
+    template <T Code, AdditionalInfo Info>
+    struct CheckErrorCode
+    {
+        constexpr static bool value = Code == SucceedValue;
+        static_assert(value);
+    };
+};
+}
