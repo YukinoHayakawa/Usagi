b6b10a1421eab95bfd0ec0270060ea023bc25d89
Yukino Hayakawa <tennencoll@gmail.com>
2021-08-24 17:11:38 +0800

    add bit & ptr util functions

---
+++ Library/Utility/BitMask.hpp
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace usagi
+{
+template <typename T>
+bool match_bit_mask(T candidate_mask, T required_bits)
+{
+    return (candidate_mask & required_bits) == required_bits;
+}
+}
---
+++ Library/Utility/Pointer.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <typeinfo>
+
+#include <Usagi/Runtime/ErrorHandling.hpp>
+
+namespace usagi
+{
+template <typename DerivedT, typename PtrT>
+DerivedT& dynamic_cast_ref_throw(const PtrT& ptr)
+{
+    auto target = dynamic_cast<DerivedT*>(ptr);
+    if (target == nullptr)
+        USAGI_THROW(std::bad_cast());
+    return *target;
+}
+}
