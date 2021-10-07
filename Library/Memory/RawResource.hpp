6ac6c3e99e82f912a97b04492d8e24e92a3f3753
Yukino Hayakawa <tennencoll@gmail.com>
2021-10-07 17:41:54 +0800

    add util classes

---
+++ Library/Memory/RawResource.hpp
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include <utility>
+
+#include "Noncopyable.hpp"
+
+namespace usagi
+{
+// Manages resource without proper RAII on its own.
+template <
+    typename InitFunc,
+    typename DestroyFunc
+>
+class RawResource : Noncopyable
+{
+    InitFunc mInitFunc;
+    DestroyFunc mDestroyFunc;
+    bool mInitialized = false;
+
+public:
+    RawResource(InitFunc init_func, DestroyFunc destroy_func)
+        : mInitFunc(std::move(init_func))
+        , mDestroyFunc(std::move(destroy_func))
+    {
+        mInitFunc();
+        mInitialized = true;
+    }
+
+    ~RawResource()
+    {
+        if(mInitialized)
+            mDestroyFunc();
+        mInitialized = false;
+    }
+
+    RawResource(RawResource &&other) noexcept
+        : mInitFunc { std::move(other.mInitFunc) }
+        , mDestroyFunc { std::move(other.mDestroyFunc) }
+        , mInitialized { other.mInitialized }
+    {
+        other.mInitialized = false;
+    }
+
+    RawResource & operator=(RawResource &&other) noexcept
+    {
+        if(this == &other)
+            return *this;
+        mInitFunc = std::move(other.mInitFunc);
+        mDestroyFunc = std::move(other.mDestroyFunc);
+        mInitialized = other.mInitialized;
+        other.mInitialized = false;
+        return *this;
+    }
+};
+}
