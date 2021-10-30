0e208235951b66a8f0e5b9d8bf2bc48831ed4cac
Yukino Hayakawa <tennencoll@gmail.com>
2021-10-30 18:45:08 +0800

    add simpleservice

---
+++ Runtime/Service/SimpleService.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <Usagi/Library/Memory/Noncopyable.hpp>
+
+namespace usagi
+{
+template <typename T>
+struct SimpleService : Noncopyable
+{
+    using ServiceT = T;
+
+    ServiceT & get_service() { return mService; }
+
+private:
+    T mService;
+};
+}
