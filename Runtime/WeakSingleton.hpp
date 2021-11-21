5c753dedaef33f68a780f8ba442e4f0bf9adda85
Yukino Hayakawa <tennencoll@gmail.com>
2021-11-21 15:51:17 +0800

    make weak singleton thread safe

--- Runtime/WeakSingleton.hpp
+++ Runtime/WeakSingleton.hpp
@@ -2,22 +2,26 @@
 
 #include <cassert>
 #include <memory>
+#include <mutex>
 
 namespace usagi
 {
 template <typename T>
 class WeakSingleton
 {
+    inline static std::recursive_mutex mMutex;
     inline static std::weak_ptr<T> mInstance;
 
 public:
     static auto lock()
     {
+        std::lock_guard lk(mMutex);
         return mInstance.lock();
     }
 
     static bool alive()
     {
+        std::lock_guard lk(mMutex);
         return !mInstance.expired();
     }
 
@@ -26,6 +30,8 @@ public:
     static auto construct(Args &&... args)
     {
         assert(!alive());
+
+        std::lock_guard lk(mMutex);
 
         auto ptr = std::make_shared<T>(std::forward<Args>(args)...);
         mInstance = ptr;
@@ -36,6 +42,7 @@ public:
     [[nodiscard]]
     static auto try_lock_construct(Args &&... args)
     {
+        std::lock_guard lk(mMutex);
         if(auto ptr = lock()) return ptr;
         return construct(std::forward<Args>(args)...);
     }
