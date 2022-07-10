d3a36dc693a40ba761c1fe50ddd5ca500b99d855
Yukino Hayakawa <tennencoll@gmail.com>
2022-07-10 17:01:47 +0800

    add maybe owned string

---
+++ Library/Utilities/MaybeOwnedString.cpp
@@ -0,0 +1,59 @@
+﻿#include "MaybeOwnedString.hpp"
+
+namespace usagi
+{
+MaybeOwnedString::MaybeOwnedString(std::string_view view)
+    : mView(view)
+{
+}
+
+MaybeOwnedString::MaybeOwnedString(const char *str)
+    : MaybeOwnedString(std::string_view(str))
+{
+}
+
+MaybeOwnedString::MaybeOwnedString(const std::string &str)
+    : MaybeOwnedString(std::string_view(str))
+{
+}
+
+MaybeOwnedString::MaybeOwnedString(std::string &&str)
+    : mStorage(std::move(str))
+    , mView(mStorage.value())
+{
+}
+
+MaybeOwnedString::MaybeOwnedString(const MaybeOwnedString &other)
+    : mStorage { other.mStorage }
+    // make sure only refer to own storage
+    , mView { mStorage.has_value() ? mStorage.value() : other.mView }
+{
+}
+
+MaybeOwnedString::MaybeOwnedString(MaybeOwnedString &&other) noexcept
+    : mStorage { std::move(other.mStorage) }
+    , mView { std::move(other.mView) }
+{
+    other.mView = std::string_view();
+}
+
+MaybeOwnedString & MaybeOwnedString::operator=(const MaybeOwnedString &other)
+{
+    if(this == &other)
+        return *this;
+    mStorage = other.mStorage;
+    mView = mStorage.has_value() ? mStorage.value() : other.mView;
+    return *this;
+}
+
+MaybeOwnedString & MaybeOwnedString::operator=(
+    MaybeOwnedString &&other) noexcept
+{
+    if(this == &other)
+        return *this;
+    mStorage = std::move(other.mStorage);
+    mView = std::move(other.mView);
+    other.mView = std::string_view();
+    return *this;
+}
+}
---
+++ Library/Utilities/MaybeOwnedString.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include <optional>
+#include <string>
+
+#include <Usagi/Concepts/Type/Types.hpp>
+
+namespace usagi
+{
+/**
+ * \brief Hold a string_view which may reference the internal storage only when
+ * a string object is passed in when constructing. Otherwise it references
+ * an external storage.
+ */
+class MaybeOwnedString
+{
+protected:
+    std::optional<std::string> mStorage;
+    std::string_view mView;
+
+public:
+    MaybeOwnedString() = default;
+
+    // copy view
+    MaybeOwnedString(std::string_view view);
+    MaybeOwnedString(const char *str);
+    MaybeOwnedString(const std::string &str);
+
+    // transfer value
+    MaybeOwnedString(std::string &&str);
+
+    ~MaybeOwnedString() = default;
+
+    // move & copy ops
+    MaybeOwnedString(const MaybeOwnedString &other);
+    MaybeOwnedString(MaybeOwnedString &&other) noexcept;
+    MaybeOwnedString & operator=(const MaybeOwnedString &other);
+    MaybeOwnedString & operator=(MaybeOwnedString &&other) noexcept;
+
+    bool is_value_owner() const { return mStorage.has_value(); }
+
+    std::string value() const { return std::string { mView }; }
+    const std::string_view & view() const { return mView; }
+};
+}
