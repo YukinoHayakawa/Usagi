020b87aa855a6a3caf3141e5f01a205e4e4a4492
Yukino Hayakawa <tennencoll@gmail.com>
2025-11-16 15:12:15 +0800

    add make_callable/unwrap_nonstatic_members/tie_nonstatic_members

---
+++ Library/Meta/Functional/Callables.hpp
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#include <tuple>
+#include <utility>
+
+#include <Usagi/Library/Meta/Reflection/StaticReflection.hpp>
+
+namespace usagi::meta::functional
+{
+// Create a callable lambda from a free template function
+template <std::meta::info FreeFuncRefl>
+constexpr auto make_callable()
+{
+    // constexpr static auto refl = ^^Func;
+    return []<typename... Args>(Args &&... args) {
+        static_assert(std::meta::can_substitute(FreeFuncRefl, { ^^Args... }));
+        // I have tried many ways to check whether the resulted object is
+        // actually callable. However, it seems that I just cannot get it to
+        // work. Anyway, the static_assert above is actually enough.
+
+        // constexpr auto func =
+        //     std::meta::substitute(FreeFuncRefl, std::vector { ^^Args... });
+        // static_assert(std::meta::is_invocable_type(
+        //     func, std::vector { ^^Args... }));
+        // static_assert(std::is_invocable_v(
+        //     [:func:], std::vector { ^^Args... }));
+
+        // a reflection of a function template cannot represent a template
+        // argument static_assert([:std::meta::substitute(^^std::is_invocable_v,
+        //    std::vector { func, ^^Args... }):]);
+        // static_assert(std::is_invocable_v<
+        //     std::meta::extract<void(Args&&...)>(func), Args...>);
+        // static_assert(std::is_invocable_v<
+        //     std::meta::extract<
+        //     std::meta::reflect_function([:func:](args...))>(func), Args...>);
+
+        // a reflection of a function template cannot represent a template
+        // argument static_assert([:std::meta::substitute(
+        //     ^^std::is_invocable_v,
+        //     std::define_static_array(std::vector { FreeFuncRefl, ^^Args... })
+        // ):]);
+
+        // ... So I gave up.
+
+        return [:std::meta::substitute(FreeFuncRefl, { ^^Args... }):](
+            std::forward<Args>(args)...
+        );
+    };
+}
+
+namespace static_tests
+{
+template <typename T>
+constexpr auto && identity(T && t)
+{
+    return std::forward<T>(t);
+}
+
+template <typename T, typename V>
+constexpr auto it_takes_two(T && t, V && v)
+{
+    return std::make_pair(t, v);
+}
+
+static_assert(make_callable<^^identity>()(1) == 1);
+static_assert(
+    make_callable<^^it_takes_two>()(1, true) == std::make_pair(1, true)
+);
+} // namespace static_tests
+} // namespace usagi::meta::functional
---
+++ Library/Meta/Reflection/Members.hpp
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include <tuple>
+#include <utility>
+
+#include <Usagi/Library/Meta/Functional/Callables.hpp>
+
+namespace usagi::meta::reflection
+{
+/*
+ * This function *unwraps* the nonstatic members of `T` with the provided
+ * `OpUnWrap` and `OpProj` functions.
+ */
+// OpUnwrap and OpProj should both be callables. For them to work, you must use
+// `reflect_constant` to get an actual function object's reflection.
+template <
+    typename T,
+    // This MUST be a reflection of something callable.
+    std::meta::info OpUnwrap,
+    // So does this, and we ae using `std::identity()` as the default fallback.
+    std::meta::info OpProj = std::meta::reflect_constant(std::identity())
+>
+constexpr auto unwrap_nonstatic_members(T & val)
+{
+    // I am yet to understand the differences between several types of
+    // `access_context`. But it seems `current()` works fine here since we are
+    // not manipulating any compound definitions.
+    constexpr static auto ctx     = std::meta::access_context::current();
+    // Use `define_static_array` to make `nonstatic_data_members_of` constexpr.
+    // This is **required**, because otherwise `[:members[I]:]` cannot be
+    // unpacked because its size is not constexpr.
+    constexpr static auto members = std::define_static_array(
+        std::meta::nonstatic_data_members_of(^^T, ctx)
+    );
+    // This is a general unwrapping function that supports value projection.
+    constexpr static auto op_unwrap =
+        [&]<std::size_t... I>(T & val, std::index_sequence<I...>) {
+            return [:OpUnwrap:]([:OpProj:](val.[:members[I]:])...);
+        };
+    return op_unwrap(val, std::make_index_sequence<members.size()>());
+}
+
+template <typename T>
+constexpr auto tie_nonstatic_members(T & val)
+{
+    return unwrap_nonstatic_members<
+        T, std::meta::reflect_constant(functional::make_callable<^^std::tie>())
+    >(val);
+}
+
+namespace static_tests
+{
+struct tie_member_test
+{
+    int  a;
+    bool b;
+};
+
+constexpr tie_member_test tie_member_test_val = [] {
+    tie_member_test temp;
+    tie_nonstatic_members(temp) = std::make_tuple(31'415, false);
+    return temp;
+}();
+
+static_assert(
+    tie_member_test_val.a == 31'415 && tie_member_test_val.b == false
+);
+} // namespace static_tests
+} // namespace usagi::meta::reflection
