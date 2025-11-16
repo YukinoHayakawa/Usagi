59ec758c9be7eaaf6d7684fd9b03a538a2f748c3
Yukino Hayakawa <tennencoll@gmail.com>
2025-11-16 15:05:39 +0800

    add closure.hpp

---
+++ Library/Meta/Reflection/Closures.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "StaticReflection.hpp"
+
+namespace usagi::meta::reflection
+{
+// https://en.cppreference.com/w/cpp/language/lambda.html
+// The lambda expression is a prvalue expression of unique
+// unnamed non-union non-aggregate class type, known as closure type.
+template <typename T>
+concept Closure =
+    !std::meta::is_union_type(^^T) && !std::meta::is_aggregate_type(^^T) &&
+    std::meta::is_class_type(^^T) && !std::meta::has_identifier(^^T);
+
+namespace static_tests
+{
+constexpr auto a_lambda = [](int) {
+};
+
+struct a_class
+{
+};
+
+static_assert(Closure<decltype(a_lambda)>);
+static_assert(!Closure<a_class>);
+} // namespace static_tests
+} // namespace usagi::meta::reflection
