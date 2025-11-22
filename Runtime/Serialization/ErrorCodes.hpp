3039d2c57881764dfe386dd2eb03a4b855406676
Yukino Hayakawa <tennencoll@gmail.com>
2025-11-22 09:45:05 +0800

    add serialization error codes

---
+++ Runtime/Serialization/ErrorCodes.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace usagi::runtime
+{
+/*
+ * Shio: As requested, this enum is defined but not used in the return type
+ * of the serialization function. The error handling is done by embedding
+ * error information within the resulting JSON/other object itself.
+ */
+enum class SerializationErrorCodes
+{
+    MaxRecursionDepthReached,
+    TypeNotMatch,
+};
+} // namespace usagi::runtime
