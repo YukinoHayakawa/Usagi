b0b8605e3528dd245c961a8f06d2f8629f0f4d5d
Yukino Hayakawa <tennencoll@gmail.com>
2025-12-17 23:25:17 +0800

    move files

--- Library/Meta/Reflection/Types/TypeList.hpp
+++ Library/Meta/Reflection/Types/TypeList.hpp
@@ -3,28 +3,30 @@
 #include <algorithm>
 #include <ranges>
 
-#include "StaticReflection.hpp"
-#include "Statics.hpp"
+#include <Usagi/Library/Meta/Reflection/StaticReflection.hpp>
+#include <Usagi/Library/Meta/Reflection/MakeStatics.hpp>
 
 namespace usagi::meta::reflection
 {
 template <std::meta::info SortFunc, typename... Ts>
-    requires(std::meta::is_function(SortFunc))
+    requires (std::meta::is_function(SortFunc))
 consteval auto sorted_types()
 {
-    std::array<std::meta::info, sizeof...(Ts)> sorted_types {^^Ts...};
-    std::ranges::sort(sorted_types, std::ranges::less {}, [](auto && refl) {
+    std::array<std::meta::info, sizeof...(Ts)> sorted_types { ^^Ts... };
+    std::ranges::sort(sorted_types, std::ranges::less { }, [](auto && refl) {
         return [:SortFunc:](refl);
     });
     return sorted_types;
 }
 
-template <typename... Ts> consteval auto sorted_types_by_names()
+template <typename... Ts>
+consteval auto sorted_types_by_names()
 {
     return sorted_types<^^std::meta::display_string_of, Ts...>();
 }
 
-template <typename... Ts> consteval auto unique_type_list_sorted_by_names()
+template <typename... Ts>
+consteval auto unique_type_list_sorted_by_names()
 {
     // todo: bug - unique somehow returns an empty subrange, so we gotta do it
     //   ourselves.
@@ -32,7 +34,7 @@ template <typename... Ts> consteval auto unique_type_list_sorted_by_names()
     // return std::define_static_array(unique_range);
 
     constexpr auto sorted_types_ = sorted_types_by_names<Ts...>();
-    std::array<std::meta::info, sizeof...(Ts)> unique_types {};
+    std::array<std::meta::info, sizeof...(Ts)> unique_types { };
     std::size_t                                num_unique_types = 0;
     std::size_t                                i                = 0;
     template for(constexpr auto t : sorted_types_)
@@ -57,9 +59,10 @@ template <typename... Ts> consteval auto unique_type_list_sorted_by_names()
     return make_static_subarray(unique_types, num_unique_types);
 }
 
-template <typename T, typename... Ts> consteval bool is_type_in_list()
+template <typename T, typename... Ts>
+consteval bool is_type_in_list()
 {
-    constexpr std::array types {^^Ts...};
+    constexpr std::array types { ^^Ts... };
     return std::ranges::find(types, ^^T) != types.end();
 }
 
@@ -90,7 +93,7 @@ consteval auto get_dealiased_type_or_template()
 // Refl has to be something you want to confirm about.
 // CmpRefl has to be a reflection of a type/type alias/template.
 template <std::meta::info Refl, std::meta::info CmpRefl>
-    requires(TypeOrTemplate<CmpRefl> || TemplateInstantiation<CmpRefl>)
+    requires (TypeOrTemplate<CmpRefl> || TemplateInstantiation<CmpRefl>)
 consteval bool is_type_or_template_instantiation_of()
 {
     if constexpr(TypeOrTypeAlias<Refl> && !TemplateInstantiation<Refl>)
@@ -114,7 +117,8 @@ consteval bool is_type_or_template_instantiation_of()
 
 namespace static_tests
 {
-template <typename> struct _meta_type_x
+template <typename>
+struct _meta_type_x
 {
 };
 
