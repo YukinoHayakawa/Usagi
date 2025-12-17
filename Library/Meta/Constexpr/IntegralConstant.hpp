76e27191475dfd035a00a9dfb8e2552a7c9d28cc
Yukino Hayakawa <tennencoll@gmail.com>
2025-12-18 03:52:10 +0800

    constexpr value lifting

---
+++ Library/Meta/Constexpr/IntegralConstant.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace usagi::meta
+{
+template <typename T, T Val>
+struct IntegralConstant
+{
+    using type               = T;
+    static constexpr T value = Val;
+};
+
+template <auto Val>
+consteval auto lift()
+{
+    return IntegralConstant<decltype(Val), Val>();
+}
+
+namespace static_tests
+{
+consteval
+{
+    constexpr auto op = []<int I>(IntegralConstant<int, I>) { return I; };
+    static_assert(lift<3>().value == 3);
+    static_assert(op(lift<123>()) == 123);
+}
+} // namespace static_tests
+} // namespace usagi::meta
