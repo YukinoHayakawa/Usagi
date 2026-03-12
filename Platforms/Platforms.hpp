aae33ca4f8e7de4809e9b1357ecd07d15095a137
Yukino Hayakawa <tennencoll@gmail.com>
2026-03-12 14:09:35 +0800

    disable clang-format for USAGI_PLATFORM_DEPENDENT

--- Platforms/Platforms.hpp
+++ Platforms/Platforms.hpp
@@ -5,8 +5,10 @@ namespace usagi
 constexpr bool platform_dependent = true;
 } // namespace usagi
 
+// clang-format off
 #ifdef __RESHARPER__
     #define USAGI_PLATFORM_DEPENDENT
 #else
-    #define USAGI_PLATFORM_DEPENDENT [[= usagi::platform_dependent]]
+    #define USAGI_PLATFORM_DEPENDENT [[=usagi::platform_dependent]]
 #endif
+// clang-format on
